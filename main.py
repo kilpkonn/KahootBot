06b96923ab652fa739643df2032f199278563928
taannu <tavo.annus@gmail.com>
2019-01-13 15:13:49 +0200

    Implement help command

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 """Main."""
-from colorama import init
+from colorama import init, Fore
 
 from kahoot_manager import KahootManager
+
+
+def print_help():
+    """Print help."""
+    print(f"""{Fore.LIGHTCYAN_EX}Commands:
+    {Fore.MAGENTA}help {Fore.GREEN} -> {Fore.WHITE}Print help to console.
+    {Fore.MAGENTA}exit {Fore.GREEN}-> {Fore.RED}Exit program!""")
 
 
 def main():
@@ -13,6 +20,9 @@ def main():
         manager_thread.input_queue.put(data)
         if data.lower() == "exit":
             break
+        elif data.lower() == "help":
+            print_help()
+
     manager_thread.join()
 
 
